9cdd1f51f137ab464cfb7b2e861b2a8b8e97a182
Renan Leonello Castro <renanleonellocastro@gmail.com>
2020-05-18 17:50:05 -0300

    Designed the main screen

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 
 import sys
-from gui import Ui_Form  # importing our generated file
+from gui import Ui_Gui
 from PySide2 import QtWidgets
 
-class Gui(QtWidgets.QWidget, Ui_Form):
+class Gui(QtWidgets.QWidget, Ui_Gui):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
+        self.center()
+
+    def center(self):
+        frame = self.frameGeometry()
+        centerPosition = QtWidgets.QDesktopWidget().availableGeometry().center()
+        frame.moveCenter(centerPosition)
+        self.move(frame.topLeft())
 
 app = QtWidgets.QApplication(sys.argv)
+app.setStyle('Fusion')
 application = Gui()
 application.show()
 app.exec_()
