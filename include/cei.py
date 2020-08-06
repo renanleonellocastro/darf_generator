06e5e0d97c0b2b5d69378b5ea5a59e6b13242bcf
Renan Leonello Castro <renanleonellocastro@gmail.com>
2020-08-05 21:40:59 -0300

    Added a crawler to get the wallet in a date in cei b3 system

--- include/cei.py
+++ include/cei.py
@@ -10,10 +10,18 @@ class Cei():
 #Definition of Class Constants
 #----------------------------------------------------------------------------------------------------------------------
     __login_endpoint = "https://cei.b3.com.br/cei_responsivo/login.aspx"
+    __wallet_endpoint = "https://cei.b3.com.br/CEI_Responsivo/ConsultarCarteiraAtivos.aspx"
     __stocks_extract_endpoint = "https://cei.b3.com.br/CEI_Responsivo/negociacao-de-ativos.aspx"
     __login_data = {"__EVENTTARGET": "", "__EVENTARGUMENT": "", "ctl00$ContentPlaceHolder1$txtLogin": "",
                 "ctl00$ContentPlaceHolder1$txtSenha": "", "__VIEWSTATEGENERATOR": "", "_ASYNCPOST": True,
                 "__EVENTVALIDATION": "", "__VIEWSTATE": "", "ctl00$ContentPlaceHolder1$btnLogar": "Entrar"}
+    __wallet_data = {"ctl00$ContentPlaceHolder1$ToolkitScriptManager1":\
+                    "ctl00$ContentPlaceHolder1$updFiltro|ctl00$ContentPlaceHolder1$ddlAgentes",
+		            "ctl00_ContentPlaceHolder1_ToolkitScriptManager1_HiddenField": "", "__ASYNCPOST": True,
+		            "__EVENTTARGET": "ctl00$ContentPlaceHolder1$ddlAgentes", "__EVENTARGUMENT": "", "__LASTFOCUS": "",
+		            "__VIEWSTATEGENERATOR": "", "__EVENTVALIDATION": "", "__VIEWSTATE": "",
+                    "ctl00$ContentPlaceHolder1$txtData": "", "ctl00$ContentPlaceHolder1$ddlContas": "0",
+                    "ctl00$ContentPlaceHolder1$ddlAgentes": ""}
     __extract_data = {"ctl00$ContentPlaceHolder1$ToolkitScriptManager1":\
                     "ctl00$ContentPlaceHolder1$updFiltro|ctl00$ContentPlaceHolder1$ddlAgentes",
 		            "ctl00_ContentPlaceHolder1_ToolkitScriptManager1_HiddenField": "", "__ASYNCPOST": True,
@@ -59,6 +67,39 @@ class Cei():
             logging.error('User %s failed to log in!', cpf)
             raise RuntimeError('User %s failed to log in!' %cpf)
 
+# Get the user wallet of purchased stocks and fis
+#----------------------------------------------------------------------------------------------------------------------
+    def get_wallet(self, search_date):
+        wallet_date = ("%02d/%02d/%04d" %(search_date.day, search_date.month, search_date.year))
+        logging.debug('Getting the wallet in %s in Cei...', wallet_date)
+        stocks = []
+        stock = {}
+
+        if not self.__logged_in:
+            logging.error('User not logged in!')
+            raise RuntimeError('User not logged in!')
+
+        try:
+            self.__web.executeGet(self.__wallet_endpoint)
+            institutions = self.__web.getOptionsValuesFromSelection('ctl00_ContentPlaceHolder1_ddlAgentes')
+            institutions.remove('0')
+
+            for institution in institutions:
+                stock['institution'] = institution
+                accounts = self.__get_accounts_from_institution_in_wallet_page(institution, wallet_date)
+
+                for account in accounts:
+                    stock['account'] = account
+                    stock['extract'] = \
+                        self.__get_stocks_from_account_and_date_in_wallet_page(institution, account, wallet_date)
+                    stocks.append(stock)
+
+            return stocks
+
+        except:
+            logging.error('Failed to get the user wallet!')
+            raise RuntimeError('Failed to get the user wallet!')
+
 # Get the user extract of purchased and sold stocks and fis
 #----------------------------------------------------------------------------------------------------------------------
     def get_extract(self, date_begin, date_end):
@@ -79,12 +120,13 @@ class Cei():
 
             for institution in institutions:
                 transaction['institution'] = institution
-                accounts = self.__get_accounts_from_institution(institution, start_date, end_date)
+                accounts = self.__get_accounts_from_institution_in_extract_page(institution, start_date, end_date)
 
                 for account in accounts:
                     transaction['account'] = account
                     transaction['extract'] = \
-                        self.__get_transactions_from_account_and_period(institution, account, start_date, end_date)
+                        self.__get_transactions_from_account_and_period_in_extract_page(institution, account,
+                            start_date, end_date)
                     transactions.append(transaction)
 
             return transactions
@@ -93,9 +135,65 @@ class Cei():
             logging.error('Failed to get the user extract!')
             raise RuntimeError('Failed to get the user extract!')
 
-# Get the accounts of an user registered institution
+# Get the accounts of an user registered institution in the wallet page
 #----------------------------------------------------------------------------------------------------------------------
-    def __get_accounts_from_institution(self, institution, start_date, end_date):
+    def __get_accounts_from_institution_in_wallet_page(self, institution, wallet_date):
+        logging.debug('Getting the account from institution %s...', institution)
+        self.__wallet_data["__VIEWSTATE"] = self.__web.getValueFromPage('#__VIEWSTATE')
+        self.__wallet_data["__VIEWSTATEGENERATOR"] = self.__web.getValueFromPage('#__VIEWSTATEGENERATOR')
+        self.__wallet_data["__EVENTVALIDATION"] = self.__web.getValueFromPage('#__EVENTVALIDATION')
+        self.__wallet_data["ctl00$ContentPlaceHolder1$ddlContas"] = "0"
+        self.__wallet_data["ctl00$ContentPlaceHolder1$txtData"] = wallet_date
+        self.__wallet_data["ctl00$ContentPlaceHolder1$ddlAgentes"] = institution
+        self.__wallet_data["ctl00$ContentPlaceHolder1$ToolkitScriptManager1"] = \
+            "ctl00$ContentPlaceHolder1$updFiltro|ctl00$ContentPlaceHolder1$ddlAgentes"
+        self.__web.executePost(self.__wallet_endpoint, self.__wallet_data)
+        return self.__web.getOptionsValuesFromSelection('ctl00_ContentPlaceHolder1_ddlContas')
+
+# Get the purchased stocks from an account and a date in the wallet page
+#----------------------------------------------------------------------------------------------------------------------
+    def __get_stocks_from_account_and_date_in_wallet_page(self, institution, account, wallet_date):
+        logging.debug('Getting the stocks from account %s...', account)
+        self.__wallet_data["__VIEWSTATE"] = self.__web.getValueFromPage('#__VIEWSTATE')
+        self.__wallet_data["__VIEWSTATEGENERATOR"] = self.__web.getValueFromPage('#__VIEWSTATEGENERATOR')
+        self.__wallet_data["__EVENTVALIDATION"] = self.__web.getValueFromPage('#__EVENTVALIDATION')
+        self.__wallet_data["ctl00$ContentPlaceHolder1$ddlAgentes"] = institution
+        self.__wallet_data["ctl00$ContentPlaceHolder1$ddlContas"] = account
+        self.__wallet_data["ctl00$ContentPlaceHolder1$txtData"] = wallet_date
+        self.__wallet_data["ctl00$ContentPlaceHolder1$btnConsultar"] = "Consultar"
+        self.__wallet_data["ctl00$ContentPlaceHolder1$ToolkitScriptManager1"] = \
+            "ctl00$ContentPlaceHolder1$updFiltro|ctl00$ContentPlaceHolder1$btnConsultar"
+        self.__web.executePost(self.__wallet_endpoint, self.__wallet_data)
+        extract = self.__read_stocks_in_wallet_page()
+        del self.__wallet_data["ctl00$ContentPlaceHolder1$btnConsultar"]
+        return extract
+
+# Read the stocks information from wallet page
+#----------------------------------------------------------------------------------------------------------------------
+    def __read_stocks_in_wallet_page(self):
+        logging.debug('Reading the stocks information from wallet page...')
+        result = []
+        table_id = \
+            "ctl00_ContentPlaceHolder1_rptAgenteContaMercado_ctl00_rptContaMercado_ctl00_rprCarteira_ctl00_grdCarteira"
+        raw_stocks = self.__web.getTextFromEachElementOfTable(table_id, 1)
+
+        for item in raw_stocks:
+            operation = {}
+            operation['company'] = item[0]
+            operation['type'] = item[1]
+            operation['code'] = item[2]
+            operation['fullcode'] = item[3]
+            operation['price'] = item[4]
+            operation['quantity'] = item[5]
+            operation['factor'] = item[6]
+            operation['total'] = item[7]
+            result.append(operation)
+
+        return result
+
+# Get the accounts of an user registered institution in the extract page
+#----------------------------------------------------------------------------------------------------------------------
+    def __get_accounts_from_institution_in_extract_page(self, institution, start_date, end_date):
         logging.debug('Getting the account from institution %s...', institution)
         self.__extract_data["__VIEWSTATE"] = self.__web.getValueFromPage('#__VIEWSTATE')
         self.__extract_data["__VIEWSTATEGENERATOR"] = self.__web.getValueFromPage('#__VIEWSTATEGENERATOR')
@@ -107,9 +205,9 @@ class Cei():
         self.__web.executePost(self.__stocks_extract_endpoint, self.__extract_data)
         return self.__web.getOptionsValuesFromSelection('ctl00_ContentPlaceHolder1_ddlContas')
 
-# Get the stocks transactions from an account and a period
+# Get the stocks transactions from an account and a period in the extract page
 #----------------------------------------------------------------------------------------------------------------------
-    def __get_transactions_from_account_and_period(self, institution, account, start_date, end_date):
+    def __get_transactions_from_account_and_period_in_extract_page(self, institution, account, start_date, end_date):
         logging.debug('Getting the transactions from account %s...', account)
         self.__extract_data["__VIEWSTATE"] = self.__web.getValueFromPage('#__VIEWSTATE')
         self.__extract_data["__VIEWSTATEGENERATOR"] = self.__web.getValueFromPage('#__VIEWSTATEGENERATOR')
@@ -120,15 +218,15 @@ class Cei():
         self.__extract_data["ctl00$ContentPlaceHolder1$txtDataAteBolsa"] = end_date
         self.__extract_data["ctl00$ContentPlaceHolder1$btnConsultar"] = "Consultar"
         self.__web.executePost(self.__stocks_extract_endpoint, self.__extract_data)
-        extract = self.__read_extract_in_page()
-        self.__reload_transactions_page(institution, account, start_date, end_date)
+        extract = self.__read_transactions_in_extract_page()
+        self.__reload_transactions_in_extract_page(institution, account, start_date, end_date)
         del self.__extract_data["ctl00$ContentPlaceHolder1$btnConsultar"]
         return extract
 
-# Reload the stock transactions page
+# Reload the extract page
 #----------------------------------------------------------------------------------------------------------------------
-    def __reload_transactions_page(self, institution, account, start_date, end_date):
-        logging.debug('Reloading the transactions page...')
+    def __reload_transactions_in_extract_page(self, institution, account, start_date, end_date):
+        logging.debug('Reloading the extract page...')
         self.__extract_data["__VIEWSTATE"] = self.__web.getValueFromPage('#__VIEWSTATE')
         self.__extract_data["__VIEWSTATEGENERATOR"] = self.__web.getValueFromPage('#__VIEWSTATEGENERATOR')
         self.__extract_data["__EVENTVALIDATION"] = self.__web.getValueFromPage('#__EVENTVALIDATION')
@@ -139,13 +237,13 @@ class Cei():
         self.__extract_data["ctl00$ContentPlaceHolder1$btnConsultar"] = "Nova Consulta"
         self.__web.executePost(self.__stocks_extract_endpoint, self.__extract_data)
 
-# Read the transactions extract information from transactions page
+# Read the transactions information from extract page
 #----------------------------------------------------------------------------------------------------------------------
-    def __read_extract_in_page(self):
-        logging.debug('Reading the extract information from transactions page...')
+    def __read_transactions_in_extract_page(self):
+        logging.debug('Reading the transactions information in the extract page...')
         result = []
         table_id = "ctl00_ContentPlaceHolder1_rptAgenteBolsa_ctl00_rptContaBolsa_ctl00_pnAtivosNegociados"
-        raw_extract = self.__web.getTextFromEachElementOfTable(table_id)
+        raw_extract = self.__web.getTextFromEachElementOfTable(table_id, 0)
 
         for item in raw_extract:
             operation = {}
